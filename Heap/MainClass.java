e72488be945929f679031fb55b8d80fa11603fc0
Äsif parvez <asifparvezsarker26@gmail.com>
2019-06-24 09:12:07 +0600

    heap data structure is added

---
+++ Heap/MainClass.java
@@ -0,0 +1,20 @@
+package datastructure;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        Heap heap = new Heap(10);
+
+        heap.insert(10);
+        heap.insert(100);
+        heap.insert(-10);
+
+        heap.heapsort();
+
+        System.out.println();
+        System.out.println(heap.getMax());
+
+    }
+
+}
