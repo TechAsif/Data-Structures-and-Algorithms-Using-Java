337353934413dcfc2f2a23da60a6c078efc9bf77
Äsif parvez <asifparvezsarker26@gmail.com>
2019-07-13 10:46:31 +0600

    Math and logic and object oriented design is added

---
+++ Concept and algorithm/ObjectOrientedDesign/FactoryDesignPattern.java
@@ -0,0 +1,3 @@
+Tutorials point video and source code
+
+https://www.youtube.com/watch?v=QKtUB_cI1JM
---
+++ Concept and algorithm/ObjectOrientedDesign/SingletonDesignPattern.java
@@ -0,0 +1,3 @@
+Tutorials point video and source code
+
+https://www.youtube.com/watch?v=p8nMZUCYoJQ
